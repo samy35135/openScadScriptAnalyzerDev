40859182a3c9a117d5d0629a09daf763f3cfd7a1
Francois <francois.esnault53@gmail.com>
2015-01-10 14:34:58 +0100

    Regex

--- server/api/thing/thingiverse.controller.js
+++ server/api/thing/thingiverse.controller.js
@@ -56,6 +56,82 @@ exports.list = function(req, res){
   thingiverseService.list(req.params.tag, req.params.page, function(err, things){
     logger.info('Fichier thingiverse.controller.js | fonction list');
 
+    var file = "";
+
+    if(things.results.length == 1){
+
+      for(var index = 0 ; index < things.results[0].files.length ; index ++){
+
+        if(things.results[0].files[index].name.indexOf(".scad") != -1){
+          file = things.results[0].files[index].content;
+        }
+      }
+              
+      var out = [];
+
+      //not tab in file
+
+      if(file.indexOf("/* [") == -1){
+        console.log("no tab in file");
+
+        out = getParameters(file);
+
+      }else{
+
+        // Ne récupérer que la partie qui concerne les paramètres (ie : au dessus du premier module)
+
+      var textParams = /^module \w+\(.*?\)/gm
+      var textNoHidden = /^\/\*(?:\s)?(?:\[)?(?:\s)?Hidden(?:\s)?(?:\])?(?:\s)?\*\//gm
+
+      var resultsTextParams = file.split(textParams);
+      file = resultsTextParams[0];
+
+      var resultsTextNoHidden = file.split(textNoHidden);
+      file = resultsTextNoHidden[0];
+
+      //console.log(file);
+
+        var diffTab = /^\/\*(?:\s)?(?:\[)(?:\s)?.*(?:\s)?(?:\])(?:\s)?\*\//gm; 
+        var diffTabName = /^(?:\/\*(?:\s)?(?:\[)(?:\s)?(.*)(?:\s)?(?:\])(?:\s)?\*\/)/gm; 
+
+        var tabs = file.split(diffTab);
+        var tabsName;
+
+
+        var i = 1;
+        while ((tabsName = diffTabName.exec(file)) != null) {
+          if (tabsName.index === diffTabName.lastIndex) {
+              diffTabName.lastIndex++;
+          }
+
+          if(tabsName.index){
+              console.log(tabsName[1]);
+              console.log(tabs[i]);
+              if(tabs.length == 0) {
+                  out.push({ TabName : "Global", Parameters : getParameters(tabs[0]) });
+              } else {
+                  out.push({ TabName : tabsName[1], Parameters : getParameters(tabs[i]) });
+              }
+          }
+          i++;
+        }
+
+       /* if(tabs.length == 0) {
+            out.push({ TabName : "Global", Parameters : getParameters(tabs[i]) });
+        } else {
+            for(var i = 0; i < tabs.length ; i++){
+              out.push({ TabName : tabsName[i], Parameters : getParameters(tabs[i+1]) });
+            }
+        }*/
+           
+
+      }
+
+      console.log(JSON.stringify(out,null,4));
+    }else{
+      console.log("too many things");
+    }
+
     if(err) { return handleError(res, err); }
     return res.json(200, things);
   })
@@ -110,4 +186,153 @@ function getComplexScadList(regExp){
 function handleError(res, err) {
   logger.info('Fichier thingiverse.controller.js | fonction handleError');
   return res.send(500, err);
+}
+
+function getParameters(textFile){
+
+      //console.log(textFile);
+            
+      var file = {};
+      file.thingParams = {};
+      file.thingParams.affectation = [];
+      file.thingParams.sliders = [];
+      file.thingParams.dropdown = [];
+      file.thingParams.imageToSurface = [];
+      file.thingParams.imageToArray = [];
+      file.thingParams.polygons = [];
+
+      //regexp
+      var affectation = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=(?:\s)?(?:"|')?(?:([-+]?[0-9]*\.?[0-9]+|(?:\w|\s)+))(?:"|')?(?:\s)?;(?: )?(?:\/\/(?:\s))?((?!\[).)*$/gm;
+      var sliders = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=(?:\s)?([-+]?[0-9]*\.?[0-9]+)(?:\s)?;(?:\s)?\/\/(?:\s)?\[([-+]?[0-9]*\.?[0-9]+)\:([-+]?[0-9]*\.?[0-9]+)\]/gm; 
+      var dropdown = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=\s*(?:"|')?(?:([-+]?[0-9]*\.?[0-9]+|(?:\w|\s)+))(?:"|')?;\s*\/\/\s*\[((?:(?:\d+|\w+)?(?:\:)?(?:(?:\w+|\s)+),)(?:(?:\d+|\w+)?(?:\:)?(?:(?:\w+|\s)+)(?:,)?)+)\]/gm;
+      var imgToSurface = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=(?:\s)?(?:(?:"|')((?:\w|\-)+\.\w+)(?:"|'))(?:\s)?;(?:\s)?\/\/(?:\s)?\[image_surface(?:\s)?:(?:\s)?(\d+)x(\d+)\]/gm; 
+      var imgToArray = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=(?:\s)?\[((?:[-+]?[0-9]*\.?[0-9]+|,|\s)+)\](?:\s)?;(?:\s)?\/\/(?:\s)?\[image_array(?:\s)?:(?:\s)?(\d+)x(\d+)\]/gm; 
+      var polygons = /^(?:\/\/\s?(.+)\s+)?(?:^([^\/\/]\w+))(?:\s)?\=(?:\s)?\[(?:\s)?(\[.*](?:\s)?](?:\s)?),\[(?:\s)?(\[.*](?:\s)?)(?:\s)?](?:\s)?];(?:\s)?\/\/(?:\s)?\[draw_polygon:(\d+)x(\d+)\]/gm;
+
+      var m;
+      var i = 0;
+  
+      
+      //console.log(textFile);
+  
+      while ((m = affectation.exec(textFile)) != null) {
+          if (m.index === affectation.lastIndex) {
+
+              affectation.lastIndex++;
+          }
+
+          if(m.index){
+            file.thingParams.affectation[i] = {
+              name : m[2].replace(/(\n|\r)/gm,""), 
+              value : m[3],
+               description : m[1]
+                };
+            i++;
+          }
+      }
+  
+      
+      i = 0;
+      while ((m = sliders.exec(textFile)) != null) {
+          if (m.index === sliders.lastIndex) {
+              sliders.lastIndex++;
+          }
+          if(m.index){
+            file.thingParams.sliders[i] = { 
+                name : m[2].replace(/(\n|\r)/gm,""),
+                min : m[4], 
+                def : m[3], 
+                max : m[5],
+                description : m[1]
+            };
+            i++;
+          }
+      }
+
+      i = 0;
+
+      while ((m = dropdown.exec(textFile)) != null) {
+          if (m.index === dropdown.lastIndex) {
+              dropdown.lastIndex++;
+          }
+          if(m.index){
+            file.thingParams.dropdown[i] = {
+                name : m[2].replace(/(\n|\r)/gm,""),
+                def : m[3],
+                values : JSON.stringify(m[4].split(",")),
+                description : m[1]
+            };
+            i++;
+          }
+      }
+
+      i = 0;
+
+       while ((m = imgToSurface.exec(textFile)) != null) {
+
+          if (m.index === imgToSurface.lastIndex) {
+
+              imgToSurface.lastIndex++;
+          }
+
+          if(m.index){
+            file.thingParams.imageToSurface[i] = { 
+              name : m[2].replace(/(\n|\r)/gm,""), 
+              file : m[3], 
+              width : m[4],
+              height : m[5],
+              description : m[1]
+            };
+            i++;
+          }
+          
+      }
+
+      i = 0;
+
+      while ((m = imgToArray.exec(textFile)) != null) {
+
+          if (m.index === imgToArray.lastIndex) {
+
+              imgToArray.lastIndex++;
+          }
+
+          if(m.index){
+            file.thingParams.imageToArray[i] = { 
+              name : m[2].replace(/(\n|\r)/gm,""), 
+              points : JSON.stringify(m[3].split(",")), 
+              paths : m[4],
+              cols : m[5],
+              description : m[1]
+            };
+            i++;
+          }
+      }
+
+      i = 0;
+
+      while ((m = polygons.exec(textFile)) != null) {
+
+          if (m.index === polygons.lastIndex) {
+
+              polygons.lastIndex++;
+          }
+
+          if(m.index){
+            file.thingParams.imageToArray[i] = { 
+              name : m[2].replace(/(\n|\r)/gm,""), 
+              array : m[3], 
+              rows : m[4],
+              width : m[5],
+              height : m[6],
+              description : m[1]
+            };
+            i++;
+          }
+      }
+      
+      
+  
+      return file;
+    
 }
